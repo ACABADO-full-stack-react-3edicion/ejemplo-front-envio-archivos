f19d5c665212fab5e355f171b0c2b3d54e19178a
Mario González <mario@mariogl.com>
2021-07-20 10:34:23 +0200

    Envío de form-data a la API

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import { useState } from "react";
 
 function App() {
   const [formulario, setFormulario] = useState(true);
-  const enviarForm = (e) => {
+  const [datosForm, setDatosForm] = useState({
+    nombre: "",
+    imagen: "",
+  });
+  const enviarForm = async (e) => {
     e.preventDefault();
+    const datos = new FormData();
+    datos.append("imagen", datosForm.imagen);
+    datos.append("nombre", datosForm.nombre);
+    const resp = await fetch("http://localhost:4000/usuarios/registro", {
+      method: "POST",
+      body: datos,
+    });
+    if (resp.ok) {
+      return setFormulario(false);
+    }
+    console.log("Algo ha petado");
+  };
+  const changeDato = (e) => {
+    console.log(e.target.files);
+    setDatosForm({
+      ...datosForm,
+      [e.target.id]:
+        e.target.type === "file" ? e.target.files[0] : e.target.value,
+    });
   };
   return (
     <section className="contenedor">
@@ -11,7 +34,13 @@ function App() {
         <form onSubmit={enviarForm} autoComplete="off">
           <div className="grupo">
             <label htmlFor="nombre">Nombre:</label>
-            <input id="nombre" type="text" className="form-control"></input>
+            <input
+              id="nombre"
+              type="text"
+              className="form-control"
+              value={datosForm.nombre}
+              onChange={changeDato}
+            ></input>
           </div>
           <div className="grupo">
             <label htmlFor="imagen" className="custom-file-label">
@@ -21,6 +50,7 @@ function App() {
               id="imagen"
               type="file"
               className="custom-file-input"
+              onChange={changeDato}
             ></input>
           </div>
           <div className="grupo">
